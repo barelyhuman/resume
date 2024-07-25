11c5358e091982c2a0f5c91a5469b03fb38e3472
Reaper Gelera <ahoy@barelyhuman.dev>
2024-07-25 12:24:17 +0530

    ci: pages

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import preact from "@preact/preset-vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: process.env.BASE_URL,
   plugins: [
     preact({
       prerender: {
